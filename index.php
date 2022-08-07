1a1d9ec1005509292086671aa8d2b8a60853a8af
alican <aliicann.yildirim@hotmail.com>
2022-08-07 14:31:10 +0200

    Javascript added

--- index.php
+++ index.php
@@ -11,8 +11,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Title</title>
+    <script src="js/cookies.js"></script>
 </head>
 <body>
-    
+
 </body>
 </html>
